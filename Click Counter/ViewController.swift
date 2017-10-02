a742e9101de8ac504b4964cc501fa7fd0bf18041
Mithun Kumar S R <rmithun@amazon.com>
2017-10-02 20:08:23 +0530

    Changes in story board label

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -10,21 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var count = 0
+    @IBOutlet var label:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Label
-        var label = UILabel()
-        label.frame = CGRect.init(x: 150, y: 150, width: 60, height: 60)
-        label.text = "0"
-        self.view.addSubview(label)
-        
-        // Button
-        var button = UIButton()
-        button.frame = CGRect.init(x: 150, y: 250, width: 60, height: 60)
-        button.setTitle("Click", for: .normal)
-        button.setTitleColor(UIColor.blue, for: .normal)
-        self.view.addSubview(button)
+
+    }
+    
+    func incrementCount(){
+        self.count += 1
+        self.label.text = "\(self.count)"
     }
     
 }
